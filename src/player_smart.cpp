01c7e78214ab08468479497ef3941171dea9c81e
buster <apple8952047@gmail.com>
2022-06-21 18:41:08 +0800

    first version

--- src/player_smart.cpp
+++ src/player_smart.cpp
@@ -31,31 +31,89 @@ void read_board(std::ifstream& fin) {
     }
 }
 
-int eval() {
-	int boo = 10;
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE; j++) {
-			if (i+j == 5)
-				boo++;
+bool inside(int i, int j) {
+	return i >= 0 && j >= 0 && i < SIZE && j < SIZE;
+}
+
+// return number of contiguous discs of the input color
+// blocked indicates whether blocked at the end
+int count(int i, int j, int color, bool &blocked, int dir[2]) {
+	blocked = true;
+
+	i += dir[0];
+	j += dir[1];
+
+	int cnt = 0;
+	while (inside(i, j) && board[i][j] == color) {
+		cnt++;
+		i += dir[0];
+		j += dir[1];
+	}
+
+	if (inside(i, j) && board[i][j] == EMPTY)
+		blocked = false;
+
+	return cnt;
+}
+
+// evaluate how good is placing disc at (i, j) for the placer
+int eval_move(int i, int j, int color) {
+	int score = 0;
+
+	bool blocked1 = true, blocked2 = true;
+	int len1, len2, len;
+	for (int k = 0; k < 4; k++) {
+		len1 = count(i, j, color, blocked1, dir[2*k]);
+		len2 = count(i, j, color, blocked2, dir[2*k+1]);
+
+		len = len1 + len2 + 1;
+
+		if (len >= 5) {
+			score = RANK1;
+			break;  // win, no need to do other.
+		}
+
+		// both end blocked, no need to consider no matter how long.
+		if (blocked1 && blocked2) continue;
+
+		bool bothOpen = (!blocked1) && (!blocked2);
+		if (len == 4) {
+			if (bothOpen) score += RANK2;
+			else score += RANK3;
+		} else if (len == 3) {
+			if (bothOpen) score += RANK3;
+			else score += RANK4;
+		} else if (len == 2) {
+			score += RANK5;
+		} else if (len == 1) {
+			score += RANK6;
 		}
 	}
 
-    return 0;
+	return score;
 }
 
-int max_search(int alpha, int beta, int depth) {
+
+int eval(int i, int j) {
+	return eval_move(i, j, player) + eval_move(i, j, opponent);
+}
+
+int max_search(int alpha, int beta, int depth, int score) {
 	if (depth == 0) {
-		return eval();
+		return score;
 	}
 
 	int v = NEG_INF;
 	int vv;
+
+	int delta;
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			if (board[i][j] == EMPTY) {
 				board[i][j] = player;
+				delta = eval(i, j);
 
-				vv = min_search(alpha, beta, depth-1);
+				vv = min_search(alpha, beta, depth-1, score + delta);
 				v = max(v, vv);
 				if (v >= beta) {
 					board[i][j] = EMPTY;
@@ -71,19 +129,21 @@ int max_search(int alpha, int beta, int depth) {
 	return v;
 }
 
-int min_search(int alpha, int beta, int depth) {
+int min_search(int alpha, int beta, int depth, int score) {
 	if (depth == 0) {
-		return eval();
+		return score;
 	}
 
 	int v = INF;
 	int vv;
+	int delta;
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			if (board[i][j] == EMPTY) {
 				board[i][j] = opponent;
+				delta = eval(i, j);
 				
-				vv = max_search(alpha, beta, depth-1);
+				vv = max_search(alpha, beta, depth-1, score - delta);
 				v = min(v, vv);
 				if (v <= alpha) {
 					board[i][j] = EMPTY;
@@ -99,38 +159,31 @@ int min_search(int alpha, int beta, int depth) {
 	return v;
 }
 
-pair<int, int> search() {
-    int beta = INF;
-    int v = NEG_INF;
-	int depth = 4;
+void write_valid_spot(std::ofstream& fout) {
+	int beta = INF;
+	int v = NEG_INF;
+	int vv;
 
-    pair<int, int> pos = {0, 0};
-
-    int vv;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == EMPTY) {
+	int depth = 3;
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (board[i][j] == EMPTY) {
 				board[i][j] = player;
-
-				vv = min_search(v, beta, depth);
-				if (vv >= v) {
+				
+				vv = min_search(v, beta, depth, eval(i, j));
+				if (vv > v) {
+					fout << i << " " << j << endl;
 					v = vv;
-					pos = {i, j};
 				}
 
 				board[i][j] = EMPTY;
-            }
-        }
-    }
+			}
+		}
+	}
 
-    return pos;
-}
 
-void write_valid_spot(std::ofstream& fout) {
-	//fout << 0 << " " << 0 << endl;
-
-	pair<int, int> pos = search();
-	fout << pos.first << " " << pos.second << endl;
+	//pair<int, int> pos = search();
+	//fout << pos.first << " " << pos.second << endl;
 }
 
 int main(int, char** argv) {
--- src/player_smart.h
+++ src/player_smart.h
@@ -1,9 +1,61 @@
 #include <limits>
 
-#define FIVE 1000
+// Heuristic of the value setting is that
+// for example, 1 contigous four is approximately as good as 2 contigous three.
+
+#define RANK1 (int)1e7  // win, don't set INF in case overflow.
+#define RANK2 1<<5
+#define RANK3 1<<4
+#define RANK4 1<<3
+#define RANK5 1<<2
+#define RANK6 1
+
+/* attack
+# 1e7
+1*full 5
+
+# 2^5
+1*full open 4
+
+# 2^4
+1*half open 4
+1*full open 3
+
+# 2^3
+1*half open 3
+
+# 2^2
+1*two
+
+# 1
+1*one
+*/
+
+/* defense (block)
+place at this pos as if i'm the enemy
+gain higher score if the enemy can gain more by placing at this pos.
+
+the higher the score (i.e. the more valuable this pos is to the enemy), 
+the more damage i caused by placing at this position.
+*/
+
+
+int dir[8][2] = {
+    {0, 1},   // right
+    {0, -1},  // left
+
+    {-1, 0},  // up
+    {1, 0},   // down
+
+    {-1, -1}, // left-up
+    {1, 1},   // right-down
+
+    {1, -1},  // left-down
+    {-1, 1},  // right-up
+};
 
 int INF = std::numeric_limits<int>::max();
 int NEG_INF = std::numeric_limits<int>::min();
 
-int max_search(int alpha, int beta, int depth);
-int min_search(int alpha, int beta, int depth);
+int max_search(int alpha, int beta, int depth, int score);
+int min_search(int alpha, int beta, int depth, int score);
